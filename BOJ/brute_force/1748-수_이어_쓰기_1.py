9aa8568733dfddeb57bc7fe6bb89fffecff9f502
hwamoc <hwamok222@naver.com>
2020-07-25 03:33:18 +0900

    https://www.acmicpc.net/problem/1748

--- BOJ/brute_force/1748-수_이어_쓰기_1.py
+++ BOJ/brute_force/1748-수_이어_쓰기_1.py
@@ -5,22 +5,22 @@
 from sys import stdin, stdout
 input = stdin.readline
 
-# n = int(input())
-# l = len(str(n))
-#
-# ans = 0
-#
-# for i in range(1, l+1):
-#   stri = '9'* i
-#   inti = int(stri)
-#   if n > inti:
-#     ans += 10**(i-1) * 9 * i
-#   else:
-#     te = n - 10**(i-1)
-#     ans += te * i
-#     break
-#
-# print(ans + l)
+n = int(input())
+l = len(str(n))
+
+ans = 0
+
+for i in range(1, l+1):
+  stri = '9'* i
+  inti = int(stri)
+  if n > inti:
+    ans += 10**(i-1) * 9 * i
+  else:
+    te = n - 10**(i-1)
+    ans += te * i
+    break
+
+print(ans + l)
 
 '''
 n = int(input())
